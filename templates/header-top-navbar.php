4b28b429b6f7a1987dc541fa9efed3a95cd0cd14
Benedicte Raae <raae@bgraphic.no>
2013-09-05 21:38:43 +0200

    Hacky extra space in header

--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?>&nbsp;&nbsp;&nbsp;</a>
     </div>
 
    <nav class="collapse navbar-collapse navbar-collapse" role="navigation">
