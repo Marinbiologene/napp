f08ee458b17eeab2deb797853d507b80163f4801
Benedicte Raae <raae@bgraphic.no>
2013-09-06 01:33:43 +0200

    Order by title

--- template-species.php
+++ template-species.php
@@ -7,7 +7,7 @@ Template Name: Seafood List
 <?php get_template_part('page'); ?>
 
 <?php global $query_string; // required
-$posts = query_posts($query_string.'&posts_per_page=-1&post_type=species'); ?>
+$posts = query_posts($query_string.'&posts_per_page=-1&post_type=species&orderby=title&order=ASC'); ?>
 
 <ul class="species-collection nav">
   <?php get_template_part('loop'); ?>
