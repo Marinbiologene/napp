0079914a6b1b4a7ace18202f6d7619a97b35609c
Benedicte Raae <raae@bgraphic.no>
2013-09-06 00:35:43 +0200

    Species image styling

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'de999bd972887498992f5ddcec1bf9eb');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '93d7054d8f0a2137cffb9a8e41cd4385');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/species-image.php
+++ templates/species-image.php
@@ -1,4 +1,6 @@
 <?php if ( has_post_thumbnail() ) : ?>
-<?php the_post_thumbnail('medium', array('class' => 'thumbnail')); ?>
-<p class="small"><?php echo get_post(get_post_thumbnail_id())->post_excerpt; ?></p>
+<figure class="featured">
+  <?php the_post_thumbnail('large'); ?>
+  <figcaption><?php echo get_post(get_post_thumbnail_id())->post_excerpt; ?></figcaption>
+</figure>
 <?php endif; ?>
