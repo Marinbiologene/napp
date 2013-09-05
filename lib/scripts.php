bcaeffac5ef7a22db88da20e31b04a0af86e18f5
Benedicte Raae <raae@bgraphic.no>
2013-09-05 23:29:16 +0200

    Species styling on its way

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4843ea1f0802f77095faea20f78e0a41');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'fe568bb66659b843a27da773692e3625');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/species.php
+++ templates/species.php
@@ -1,10 +1,10 @@
 <li>
 
-  <a href="<?php the_permalink(); ?>"><?php the_title(); ?><span></span><span class="pull-right glyphicon glyphicon-chevron-right"></span></i></a>
+  <a class="<?= do_shortcode('[ct id="ct_Classifica_radio_a43a"]') ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?><span></span><span class="pull-right glyphicon glyphicon-chevron-right"></span></i></a>
   
   <article <?php post_class(); ?>>
    <header>
-     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+     <h2 class="entry-title"><?php the_title(); ?></h2>
    </header>
    <div class="entry-content">
      <?php the_content(); ?>
