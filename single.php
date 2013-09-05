b3149dd0c3556f64e3c25457e0488583cc634cc2
Benedicte Raae <raae@bgraphic.no>
2013-09-05 17:06:21 +0200

    Use species instead of content in single for species

--- single.php
+++ single.php
@@ -1,1 +1,5 @@
-<?php get_template_part('templates/content', 'single'); ?>
+<?php if(get_post_type() == 'species') : ?>
+  <?php get_template_part('templates/species', 'single'); ?>
+<?php else : ?>
+  <?php get_template_part('templates/content', 'single'); ?>
+<?php endif; ?>
--- templates/species.php
+++ templates/species.php
@@ -1,11 +1,11 @@
 <article <?php post_class(); ?>>
  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+   <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
  </header>
  <div class="entry-content">
    <?php the_content(); ?>
-    <?php get_template_part('templates/species-meta-classification', get_post_format()); ?>
-    <?php get_template_part('templates/species-meta-climate', get_post_format()); ?>
-    <?php get_template_part('templates/species-meta-badge', get_post_format()); ?>
+   <?php get_template_part('templates/species-meta-classification', get_post_format()); ?>
+   <?php get_template_part('templates/species-meta-climate', get_post_format()); ?>
+   <?php get_template_part('templates/species-meta-badge', get_post_format()); ?>
  </div>
 </article>
