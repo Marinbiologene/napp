331223853eff727d880ff45a8f0cb4786a6b5c7d
Benedicte Raae <raae@bgraphic.no>
2013-09-06 00:24:00 +0200

    Species env badges is place

--- templates/species-meta-badge.php
+++ templates/species-meta-badge.php
@@ -1,3 +1,18 @@
 <p>
-   <?php echo do_shortcode('[ct id="ct_Badges_checkbox_110e"]') ?>
+
+  <?php if (do_shortcode('[ct id="ct_Badges_checkbox_110e"]')) : ?>
+    Se etter dette merket:
+  <?php endif; ?>
+  
+</p>
+<p>     
+  <?php if (strpos(do_shortcode('[ct id="ct_Badges_checkbox_110e"]'),'KRAV') !== false ) : ?>
+    <img class="env-badge" src="<?= get_bloginfo('stylesheet_directory')?>/assets/img/napp-icon_krav.jpg" alt="Krav">
+  <?php endif; ?>
+  <?php if (strpos(do_shortcode('[ct id="ct_Badges_checkbox_110e"]'),'MSC') !== false ) : ?>
+    <img class="env-badge" src="<?= get_bloginfo('stylesheet_directory')?>/assets/img/napp-icon_msc.jpg" alt="MSC (Marine Stewardship Council)">
+  <?php endif; ?>
+  <?php if (strpos(do_shortcode('[ct id="ct_Badges_checkbox_110e"]'),'ASC') !== false ) : ?>
+    <img class="env-badge" src="<?= get_bloginfo('stylesheet_directory')?>/assets/img/napp-icon_asc.jpg">
+  <?php endif; ?>
 </p>
