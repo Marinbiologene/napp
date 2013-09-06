45c16d05bd73089dd36c166e20e7340e3167b24a
Benedicte Raae <raae@bgraphic.no>
2013-09-06 13:42:04 +0200

    Logo in navbar

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'a904cb9c801bf5900b52707409c4c3a0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '992d69bbbbd8ff4cb53212c5e5190f93');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/header-top-navbar.php
+++ templates/header-top-navbar.php
@@ -7,13 +7,13 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?>&nbsp;&nbsp;&nbsp;</a>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>/"><?php bloginfo('name'); ?></a>
     </div>
 
    <nav class="collapse navbar-collapse navbar-collapse" role="navigation">
    <?php
      if (has_nav_menu('left_main_navigation')) :
-       wp_nav_menu(array('theme_location' => 'left_main_navigation', 'menu_class' => 'nav navbar-nav navbar-left'));
+       wp_nav_menu(array('theme_location' => 'left_main_navigation', 'menu_class' => 'nav navbar-nav'));
      endif;
    ?>
                    
