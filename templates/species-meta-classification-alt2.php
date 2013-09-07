5b54e0819de584789528d5e4511da1a2e45da092
Benedicte Raae <raae@bgraphic.no>
2013-09-07 09:52:33 +0200

    Fikset skrivefeil

--- templates/species-meta-classification-alt2.php
+++ templates/species-meta-classification-alt2.php
@@ -2,7 +2,7 @@
    <?php if(do_shortcode('[ct id="ct_Classifica_radio_a43a"]') == 'Green') : ?>
       <?php the_title(); ?> er på <a href="<?= home_url() ?>/klassifisering">den grønne listen</a>; et godt valg.
    <?php elseif(do_shortcode('[ct id="ct_Classifica_radio_a43a"]') == 'Orange') : ?>
-      <?php the_title(); ?> er på <a href="<?= home_url() ?>/klassifisering">den orange listen</a>; vær kritisk.
+      <?php the_title(); ?> er på <a href="<?= home_url() ?>/klassifisering">den oransje listen</a>; vær kritisk.
    <?php elseif(do_shortcode('[ct id="ct_Classifica_radio_a43a"]') == 'Red') : ?>
       <?php the_title(); ?> er på <a href="<?= home_url() ?>/klassifisering">den røde listen</a>; et dårlig valg.
    <?php elseif(do_shortcode('[ct id="ct_Classifica_radio_a43a"]') == 'Gray') : ?>
