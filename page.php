d5028be70f63595ed84018b22458d682a94ec0ef
Benedicte Raae <raae@bgraphic.no>
2013-09-07 13:18:00 +0200

    Added article tags to element on pages and section on seafood list

--- page.php
+++ page.php
@@ -1,2 +1,6 @@
+<article <?php post_class(); ?>>
+<header>
 <?php get_template_part('templates/page', 'header'); ?>
+</header>
 <?php get_template_part('templates/content', 'page'); ?>
+</article>
--- template-species.php
+++ template-species.php
@@ -4,7 +4,11 @@ Template Name: Seafood List
 */
 ?>
 
-<?php get_template_part('page'); ?>
+<section <?php post_class(); ?>>
+<header>
+<?php get_template_part('templates/page', 'header'); ?>
+</header>
+<?php get_template_part('templates/content', 'page'); ?>
 
 <?php global $query_string; // required
 $posts = query_posts($query_string.'&posts_per_page=-1&post_type=species&orderby=title&order=ASC'); ?>
@@ -14,3 +18,5 @@ $posts = query_posts($query_string.'&posts_per_page=-1&post_type=species&orderby
 </ul>
 
 <?php wp_reset_query(); // reset the query ?>
+
+</section>
